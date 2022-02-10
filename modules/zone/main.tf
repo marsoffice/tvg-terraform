b81be9c2a8ed7d69554227436d1fc337080ffb88
Alin Florin <alin@qoffice.ro>
2022-02-10 14:02:39 +0200

    f

--- modules/zone/main.tf
+++ modules/zone/main.tf
@@ -89,7 +89,8 @@ locals {
       aikey      = "@Microsoft.KeyVault(SecretUri=${module.kvl.url}secrets/aikey/)",
       speechendpoint      = "@Microsoft.KeyVault(SecretUri=${module.kvl.url}secrets/speechendpoint/)",
       speechkey      = "@Microsoft.KeyVault(SecretUri=${module.kvl.url}secrets/speechkey/)",
-      ffmpegpath = "./libs/ffmpeg_linux"
+      ffmpegpath = "./libs/ffmpeg_linux",
+      ffprobepath = "./libs/ffprobe_linux"
     })
   )
 
