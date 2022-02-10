0801056ec1940b532b467b17b85f0ca4c3ad511c
Alin Florin <alin@qoffice.ro>
2022-02-10 14:34:34 +0200

    f

--- modules/sa/main.tf
+++ modules/sa/main.tf
@@ -19,3 +19,15 @@ resource "azurerm_storage_account" "storage_account" {
     versioning_enabled  = true
   }
 }
+
+resource "azurerm_storage_container" "jobsdatacontainer" {
+  name                  = "jobsdata"
+  storage_account_name  = azurerm_storage_account.storage_account.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_container" "editorcontainer" {
+  name                  = "editor"
+  storage_account_name  = azurerm_storage_account.storage_account.name
+  container_access_type = "private"
+}
